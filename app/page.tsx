04dd11dbc8d8afdb64dda51e346c077b9b2272cc
0xlaguna <lagunamichael2@outlook.com>
2024-06-17 17:52:28 -0600

    feat: config stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
 "use client"
 
-import { useToast } from "@/components/ui/use-toast";
-import { Button } from "@/components/ui/button";
-
+import { Button } from "@/components/ui/button"
+import { useToast } from "@/components/ui/use-toast"
 
 export default function Home() {
-  const { toast } = useToast();
+  const { toast } = useToast()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Button onClick={() => {
-          toast({
-            title: "Scheduled: Catch up",
-            description: "Friday, February 10, 2023 at 5:57 PM",
-          })
-        }}>Show Toast</Button>
+        <Button
+          onClick={() => {
+            toast({
+              title: "Scheduled: Catch up",
+              description: "Friday, February 10, 2023 at 5:57 PM",
+            })
+          }}
+        >
+          Show Toast
+        </Button>
       </div>
     </main>
-  );
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,15 @@
 import type { Config } from "tailwindcss"
+
 const { fontFamily } = require("tailwindcss/defaultTheme")
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
