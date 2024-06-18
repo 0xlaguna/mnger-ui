9e5d462b44ea2673f94d6a7bc6b55e14639884b7
0xlaguna <lagunamichael2@outlook.com>
2024-06-17 18:29:36 -0600

    feat: auth and base login

---
+++ app/dash/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <p>Hello from Dash</p>
+}
+
+export default Page
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,5 @@
-"use client"
+import { redirect } from "next/navigation"
 
-import { Button } from "@/components/ui/button"
-import { useToast } from "@/components/ui/use-toast"
-
-export default function Home() {
-  const { toast } = useToast()
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Button
-          onClick={() => {
-            toast({
-              title: "Scheduled: Catch up",
-              description: "Friday, February 10, 2023 at 5:57 PM",
-            })
-          }}
-        >
-          Show Toast
-        </Button>
-      </div>
-    </main>
-  )
+export default function IndexPage() {
+  redirect("/auth")
 }
