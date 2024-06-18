53480088315304169792e0f4983d722e580ac139
0xlaguna <lagunamichael2@outlook.com>
2024-06-17 18:10:02 -0600

    feat: structurer for fonts and react-query

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Mnger UI",
+  description: "Mnger UI",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    docs: "",
+  },
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mnger-prod.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/webapp-public/**",
+      },
+    ]
+  }
+};
 
 export default nextConfig;
