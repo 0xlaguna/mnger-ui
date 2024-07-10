cb134c263dec0be730ff505dfced2e0684a77e0f
0xlaguna <lagunamichael2@outlook.com>
2024-07-10 08:07:10 -0600

    feat: login form

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,16 +1,4 @@
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-
-import { UserAuthForm } from "./components/user-auth-form"
+import { UserLoginForm } from "./components/user-login-form"
 
 interface LayoutProps {
   children: React.ReactNode
@@ -19,27 +7,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="container relative flex h-screen flex-col items-center justify-center md:grid lg:max-w-none">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" required />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full">Sign in</Button>
-        </CardFooter>
-      </Card>
+      <UserLoginForm />
     </div>
   );
 }
