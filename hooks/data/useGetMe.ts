2caa1b3e4598184e9b140b7b73d47e9c46673079
0xlaguna <lagunamichael2@outlook.com>
2024-07-31 16:22:40 -0600

    feat: axios client and getMe hook

---
+++ hooks/data/useGetMe.ts
@@ -0,0 +1,43 @@
+"use client"
+
+import { useEffect } from "react"
+import { useQuery } from "@tanstack/react-query"
+import { useSession } from "next-auth/react"
+
+import { fetcher } from "@/lib/api/api-client"
+
+interface GeMeData {
+  email: string
+  first_name: string
+  middle_name: string
+  last_name: string
+}
+
+const useGetMe = () => {
+  const session = useSession()
+
+  useEffect(() => {}, [session])
+
+  const token = session.data?.accessToken
+  const headers = {
+    "x-session-token": token,
+  }
+
+  const { isError, isLoading, isSuccess, data, refetch } = useQuery<
+    GeMeData,
+    Error
+  >({
+    queryKey: ["users-getme"],
+    queryFn: () => fetcher<GeMeData>("/users/me", {}, headers),
+    enabled: !!token,
+  })
+
+  return {
+    getMeError: isError,
+    getMeLoading: isLoading || !token,
+    getMeSuccess: isSuccess,
+    getMeData: data,
+  }
+}
+
+export default useGetMe
---
+++ lib/api/api-client.ts
@@ -0,0 +1,40 @@
+import { AxiosResponse } from "axios"
+
+import axiosInstance from "@/config/axios/client-instance"
+
+export const fetcher = async <T>(
+  url: string,
+  params: any,
+  headers: any
+): Promise<T> => {
+  if (!headers["x-session-token"]) {
+    throw new Error("Session token is missing")
+  }
+
+  const response = await axiosInstance.get<T>(url, { params: params, headers })
+  return response.data
+}
+
+export const poster = async <T, U>(
+  url: string,
+  data: T,
+  headers: any
+): Promise<U> => {
+  if (!headers["x-session-token"]) {
+    throw new Error("Session token is missing")
+  }
+
+  const response = await axiosInstance.post<T, AxiosResponse<U>>(url, data, {
+    headers,
+  })
+  return response.data
+}
+
+export const destroy = async (url: string, params: any, headers: any) => {
+  if (!headers["x-session-token"]) {
+    throw new Error("Session token is missing")
+  }
+
+  const response = await axiosInstance.delete(url, { params, headers })
+  return response.data
+}
