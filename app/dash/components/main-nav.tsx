2e62c531502265aeee581613f70f146201e62d38
0xlaguna <lagunamichael2@outlook.com>
2024-08-08 13:10:54 -0600

    feat: add sheet nav component and remove others unused

--- app/dash/components/main-nav.tsx
+++ app/dash/components/main-nav.tsx
@@ -1,30 +1,10 @@
-"use client"
-
 import Link from "next/link"
-import { usePathname } from "next/navigation"
 import { Bell, Package2 } from "lucide-react"
-
-import { siteConfig } from "@/config/site"
 import { Button } from "@/components/ui/button"
 
+import { NavList } from "./nav-list"
+
 export function MainNav() {
-  const pathname = usePathname()
-
-  const isActive = (path: string) =>
-    pathname === path ? "text-primary" : "text-muted-foreground"
-
-  const linkList = siteConfig.mainNav.map((link) => {
-    return (
-      <Link
-        key={link.id}
-        href={link.href}
-        className={`flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${isActive(link.href)}`}
-      >
-        <link.icon className="h-4 w-4" />
-        {link.title}
-      </Link>
-    )
-  })
 
   return (
     <div className="flex h-full max-h-screen flex-col gap-2">
@@ -40,7 +20,7 @@ export function MainNav() {
       </div>
       <div className="flex-1">
         <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-          {linkList}
+          <NavList linkClassName="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary" />
         </nav>
       </div>
     </div>
--- app/dash/layout.tsx
+++ app/dash/layout.tsx
@@ -1,5 +1,6 @@
 import { GlobalSearch } from "./components/global-search"
 import { MainNav } from "./components/main-nav"
+import { SheetNav } from "./components/sheet-nav"
 import { UserNav } from "./components/user-nav"
 
 interface LayoutProps {
@@ -9,24 +10,24 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
+      <div className="bg-muted/40 hidden border-r md:block">
         <MainNav />
       </div>
       <div className="flex flex-col">
         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-          {/* <Notifications /> */}
+          <SheetNav />
           <GlobalSearch />
           <UserNav />
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <div className="flex items-center">
+          {/* <div className="flex items-center">
             <h1 className="text-lg font-semibold md:text-2xl">Dash</h1>
-          </div>
-          <div
+          </div> */}
+          {/* <div
             className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
             x-chunk="dashboard-02-chunk-1"
           >
-            {/* <div className="flex flex-col items-center gap-1 text-center">
+            <div className="flex flex-col items-center gap-1 text-center">
               <h3 className="text-2xl font-bold tracking-tight">
                 You have no products
               </h3>
@@ -34,9 +35,9 @@ export default function Layout({ children }: LayoutProps) {
                 You can start selling as soon as you add a product.
               </p>
               <Button className="mt-4">Add Product</Button>
-            </div> */}
-            {children}
-          </div>
+            </div>
+          </div> */}
+          {children}
         </main>
       </div>
     </div>
